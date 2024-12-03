77a0484c2dbf02385341df6fdd5f0548c22acb0d
redsamuray@ <redsamuray49@gmail.com>
2024-12-03 13:18:46 +0300

    bruh...

---
+++ scripts/register.js
@@ -0,0 +1,25 @@
+const database = [
+    
+]
+
+const express = require('express')
+
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.get('/', (request, response) =>{
+    response.send('Hello World');
+});
+
+app.post('/register', (request, response) =>{
+    let {email, password, login} = request.body
+    database.push({email, login, password})
+    response.send('User registered successfully!')
+});
+
+
+app.listen(3000)
+
+
